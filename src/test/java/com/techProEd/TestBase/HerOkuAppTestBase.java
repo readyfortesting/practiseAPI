0afed4757e2b8ea86368333335ba492c76d26f15
tallboy6077 <tallboy6077@gmail.com>
2022-03-20 17:20:17 +0300

    HerOkuAppTestBase

---
+++ src/test/java/com/techProEd/TestBase/HerOkuAppTestBase.java
@@ -0,0 +1,24 @@
+package com.techProEd.TestBase;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class HerOkuAppTestBase {
+
+   protected RequestSpecification spec02;
+
+   @Before
+    public void setUp(){
+
+       spec02=new RequestSpecBuilder().
+               setBaseUri("https://restful-booker.herokuapp.com").
+               build();
+
+
+
+   }
+
+
+
+}
