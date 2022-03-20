fc387b81c487983e5727c3548c0a07c1f5b5ede7
tallboy6077 <tallboy6077@gmail.com>
2022-03-20 17:19:49 +0300

    GetRequestP08

---
+++ src/test/java/com/techProEd/Day05/GetRequestP08.java
@@ -0,0 +1,86 @@
+package com.techProEd.Day05;
+
+import com.techProEd.TestBase.HerOkuAppTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class GetRequestP08 extends HerOkuAppTestBase {
+    /*  https://restful-booker.herokuapp.com/booking/5  url'ine
+    accept type'i "application/json" olan GET requesti yolladigimda
+
+    - HTTP Status kodunun 200
+    - Content type'inin "application/json" oldugunu
+    ve response body'sinin asagidaki gibi oldugunu test edin
+
+    {
+     "firstname": "Jim",
+    "lastname": "Smith",
+    "totalprice": 125,
+    "depositpaid": true,
+    "bookingdates": {
+        "checkin": "2020-12-10",
+        "checkout": "2021-12-02"
+    }
+}
+       */
+
+
+    @Test
+    public void test08(){
+
+        /********  ------JSON Path methodu ---------- *********
+        response.jsonPath(); methodu JsonPath class'indan obje ureterek
+        response uzerinden JsonPath class'indaki methodlari kullanmamizi saglar. */
+
+        spec02.pathParams("parametre1","booking","parametre2",5);
+
+        Response response=given().
+                accept("application/json").
+                spec(spec02).
+                when()
+                .get("/{parametre1}/{parametre2}");
+        response.prettyPrint();
+
+
+        //Json Path ile Dogrulama
+        JsonPath jsonPath=response.jsonPath();
+
+
+
+        //Matcher class kullanarak status code ve ContentType assert edebiliriz.
+       // response.then().assertThat().statusCode(200).contentType(ContentType.JSON);
+
+        //Matcher class kullanmak istemezsek status code ve ContentType assert icin
+        Assert.assertEquals(200,response.getStatusCode());
+
+
+        //Junit'de ONCE expected SONRA actual data yazilir.
+        //Json Path ile Dogrulama
+        Assert.assertEquals( "Jim",jsonPath.getString("firstname"));
+        Assert.assertEquals("Smith",jsonPath.getString("lastname"));
+        Assert.assertEquals(125,jsonPath.getInt("totalprice"));
+        Assert.assertEquals(true,jsonPath.getBoolean("depositpaid"));
+        Assert.assertEquals("2020-12-10",jsonPath.getString("bookingdates.checkin"));
+        Assert.assertEquals("2021-12-02",jsonPath.getString("bookingdates.checkout"));
+
+
+
+
+        /*********   TEST PASSED *******
+
+         "firstname": "Jim",
+         "lastname": "Smith",
+         "totalprice": 125,
+         "depositpaid": true,
+         "bookingdates": {
+         "checkin": "2020-12-10",
+         "checkout": "2021-12-02"
+
+         */
+    }
+}
