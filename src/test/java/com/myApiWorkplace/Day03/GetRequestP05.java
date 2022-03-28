d7f5c442f5a56c42dbf9a9376477fbb0575bbcaa
tallboy6077 <tallboy6077@gmail.com>
2022-03-28 15:03:19 +0300

    GetRequestP05.java

--- src/test/java/com/myApiWorkplace/Day03/GetRequestP05.java
+++ src/test/java/com/myApiWorkplace/Day03/GetRequestP05.java
@@ -1,8 +1,7 @@
-package com.techProEd.Day03;
+package com.myApiWorkplace.Day03;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
