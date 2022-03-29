1af6aa431dd3286b3c994b9645a6783453894e62
tallboy6077 <tallboy6077@gmail.com>
2022-03-29 18:24:55 +0300

    GetRequestP08

--- src/test/java/com/myApiWorkplace/Day05/GetRequestP08.java
+++ src/test/java/com/myApiWorkplace/Day05/GetRequestP08.java
@@ -1,6 +1,6 @@
-package com.techProEd.Day05;
+package com.myApiWorkplace.Day05;
 
-import com.techProEd.TestBase.HerokuAppTestBase;
+import com.myApiWorkplace.TestBase.HerokuAppTestBase;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Assert;
