400cdc458032ca0196c4bb9f4ec85b41f25e713f
tallboy6077 <tallboy6077@gmail.com>
2022-03-29 18:25:20 +0300

    GetRequestP09

--- src/test/java/com/myApiWorkplace/Day05/GetRequestP09.java
+++ src/test/java/com/myApiWorkplace/Day05/GetRequestP09.java
@@ -1,6 +1,6 @@
-package com.techProEd.Day05;
+package com.myApiWorkplace.Day05;
 
-import com.techProEd.TestBase.DummyTestBase;
+import com.myApiWorkplace.TestBase.DummyTestBase;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Assert;
